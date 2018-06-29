d0619ae75561fb8d4928262ed48a802e2a29e87c
lawki <prakharpalpfdr@gmail.com>
2018-06-30 01:38:48 +0530

    change colors

--- src/app/service/url.service.ts
+++ src/app/service/url.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class UrlService {
-  host = "http://localhost:3000/";
+  host = "/";//"http://localhost:3000/";
   constructor() { }
   getHostAddress(){
     return this.host;
